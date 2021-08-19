d8834a47bb225ba1d9e4576ffd919899b8453ce2
francis150 <francisdelavictoria150@gmail.com>
2021-08-19 14:02:23 -0800

    BETA v0.0.2

--- src/get-service-areas.php
+++ src/get-service-areas.php
@@ -6,9 +6,7 @@ if (isset($CONFIG['areasServed'])) {
         echo '
         <div class="service-area-wrapper" data-country="'.$serviceArea['country'].'" data-state="'.$serviceArea['state'].'" data-city-town="'.$serviceArea['cityTown'].'" data-url="'.$serviceArea['url'].'" data-zip-codes="'.implode(",",$serviceArea['zipCodes']).'">
             <div class="service-area">
-                <p><span class="name">'.$serviceArea['cityTown'].'</span> - '.$serviceArea['state'].'</p>
-                <img class="edit-btn" style="margin-left: auto;" src="'.$SERVER.'images/edit_icon.svg">
-                <img class="trash-btn" src="'.$SERVER.'images/trash_icon.svg">
+                <p><span class="name">'.$serviceArea['cityTown'].'</span> - '.$serviceArea['state'].'</p><img style="margin-left: auto;" class="trash-btn" src="'.$SERVER.'images/trash_icon.svg">
             </div>
         </div>
         ';
