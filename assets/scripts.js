a5e32bbe5a925007aa411dc11e02638945c579ed
francis150 <francisdelavictoria150@gmail.com>
2021-09-03 17:46:07 +0800

    Contact Page Subform Functionalities Done

--- assets/scripts.js
+++ assets/scripts.js
@@ -225,9 +225,75 @@ document.querySelector('.rank-main-wrapper .form-container .about-page-overlay f
 
 })
 
-/* NOTE Add Contact Page Button */
+/* NOTE Contact Page Add Button */
 document.querySelector('.rank-main-wrapper .form-container .main-form .add-contact-page-btn').addEventListener('click', () => {
     showContactPageSubform()
+})
+
+/* NOTE Contact Page Edit Button */
+document.querySelectorAll('.rank-main-wrapper .form-container .main-form .contact-pages-container .contact-page .contact-edit-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        showContactPageSubform(button.parentNode.dataset)
+    })
+})
+
+/* NOTE Contact Page Remove Button */
+document.querySelectorAll('.rank-main-wrapper .form-container .main-form .contact-pages-container .contact-page .contact-remove-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        if (confirm(`Are you sure you want to remove ${button.parentNode.dataset.url} as a Contact Page?`)) {
+            button.parentNode.parentNode.removeChild(button.parentNode)
+        }
+    })
+})
+
+/* NOTE Contact Page Submit Subform */
+document.querySelector('.rank-main-wrapper .form-container .contact-page-overlay form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const form = e.target
+
+    const contactPageWrapper = document.createElement('div')
+    contactPageWrapper.className = 'contact-page'
+
+    contactPageWrapper.dataset.key = form.url.value
+    contactPageWrapper.dataset.url = form.url.value
+
+    const urlText = document.createElement('p')
+    urlText.innerHTML = form.url.value
+    contactPageWrapper.appendChild(urlText)
+
+    const editBtn = document.createElement('button')
+    editBtn.className = 'about-edit-btn action-button'
+    editBtn.type = 'button'
+    editBtn.innerHTML = `<img src="${PLUGIN_DIR}assets/edit_icon.svg">`
+    editBtn.addEventListener('click', () => {
+        showContactPageSubform({
+            key: contactPageWrapper.dataset.key,
+            url: contactPageWrapper.dataset.url
+        })
+    })
+    contactPageWrapper.appendChild(editBtn)
+
+    const removeBtn = document.createElement('button')
+    removeBtn.className = 'about-remove-btn action-button'
+    removeBtn.type = 'button'
+    removeBtn.innerHTML = `<img src="${PLUGIN_DIR}assets/trash_icon.svg">`
+    removeBtn.addEventListener('click', () => {
+        if (confirm(`Are you sure you want to remove ${contactPageWrapper.dataset.url} as a Contact Page?`)) {
+            document.querySelector('.rank-main-wrapper .form-container .main-form .contact-pages-container').removeChild(contactPageWrapper)
+        }
+    })
+    contactPageWrapper.appendChild(removeBtn)
+
+    if (form.edit_key.value) {
+        // NOTE Update Contact Page Element with key of edit_key.value
+        document.querySelector('.rank-main-wrapper .form-container .main-form .contact-pages-container').replaceChild(contactPageWrapper, document.querySelector(`.rank-main-wrapper .form-container .main-form .contact-pages-container .contact-page[data-key="${form.edit_key.value}"]`))
+    } else {
+        // NOTE Add as a new Contact Page Element
+        document.querySelector('.rank-main-wrapper .form-container .main-form .contact-pages-container').appendChild(contactPageWrapper)
+    }
+
+    form.reset()
+    form.parentNode.style.display = 'none'
 })
 
 /* NOTE Add Service Page Button */
@@ -287,8 +353,13 @@ function showAboutPageSubform(data) {
 
 /* NOTE SHOW Contact Page Subform */
 function showContactPageSubform(data) {
+    const form = document.querySelector('.rank-main-wrapper .form-container .contact-page-overlay form')
+    form.edit_key.value = ''
+
     if (data) {
-        // TODO Load Data
+        // NOTE Load Data
+        form.edit_key.value = data.key
+        form.url.value = data.url
     }
 
     document.querySelector('.rank-main-wrapper .form-container .contact-page-overlay').style.display = 'flex'
